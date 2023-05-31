2632485e7c713c4da39db425fb44a25647b21f92
unknown <emmanuelileberi@gmail.com>
2023-05-31 18:02:43 +0200

    First Commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import data from './data.json';
 
 export default defineConfig({
-    base : '/Personal-Webpage/',
+    base : '/Personal-Webpages/',
     build: {
         rollupOptions: {
             input : {
